80584157c3a8882ba4e2efeccd35660a1d01e5cd
JackWeinstein808 <43310898+JackWeinstein808@users.noreply.github.com>
2018-09-15 22:18:51 -1000

    Update Library.java
    Example of how to use HashMap.

--- Library.java
+++ Library.java
@@ -5,11 +5,15 @@ class Library {
   public Library() {
   }
   
+  //method that gets the booms that are finished
+  //HashMap library stores the book name as a string and a boolean value of wether it is finished or not
   public void getFinishedBooks(HashMap<String, Boolean> library) {
+    //if there are no books in the library
     if (library.size() < 1 ) {
       System.out.println("Error: HashMap library is empty.");
     }
     else {
+      //getting the boolean value of the book, if true than it is finished
       for (String book : library.keySet()) {
         if ((library.get(book)) == true) {
           System.out.println("You finished " + book);
@@ -17,7 +21,7 @@ class Library {
       }
     }
   }
-  
+  //method for determining if the book is unfinished
   public void getUnfinishedBooks(HashMap<String, Boolean> library) {
     if (library.size() < 1 ) {
     	System.out.println("Error: HashMap library is empty.");
@@ -32,6 +36,7 @@ class Library {
   }
   
   public static void main(String[] args) {
+    //adding books to myBooks HashMap
     HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
     myBooks.put("Road Down The Funnel", true);
     myBooks.put("Rat: A Biology", false);
@@ -39,6 +44,7 @@ class Library {
     myBooks.put("3D Food Printing", false);
     
     Library myLibrary = new Library();
+    //calling on the 2 methods
     myLibrary.getFinishedBooks(myBooks);
     myLibrary.getUnfinishedBooks(myBooks);
   }
